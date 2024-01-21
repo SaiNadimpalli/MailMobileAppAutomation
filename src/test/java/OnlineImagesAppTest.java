43747d700ad9bfa9b92825d65966d2ad95c7acb9
sai.nadimpalli <Sai.Nadimpalli@N-able.com>
2024-01-21 22:37:30 +0000

    Add code to the repo

--- src/test/java/OnlineImagesAppTest.java
+++ src/test/java/OnlineImagesAppTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.sound.midi.InvalidMidiDataException;
 
+import org.checkerframework.checker.units.qual.s;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -17,6 +18,7 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.touch.TapOptions;
 
 public class OnlineImagesAppTest {
     	
@@ -41,24 +43,34 @@ public class OnlineImagesAppTest {
 		desiredCapabilities.setCapability("autoAcceptAlerts", true);
 
 		URL remoteUrl = new URL("http://127.0.0.1:4723/");
-		driver = new AppiumDriver(remoteUrl, desiredCapabilities);
-
-		
-
-		
+		driver = new AppiumDriver(remoteUrl, desiredCapabilities);		
 		
  }
  
    @Test
     public void goToDownloadsAndSelectArticle() throws MalformedURLException, InvalidMidiDataException, InterruptedException{
-		
+		WebElement archiveElement = driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"Tap me to open the archive\"]/android.view.ViewGroup/android.view.ViewGroup"));;   
+        archiveElement.click();
+
+        WebElement downloadSections = driver.findElement(By.xpath("//android.widget.TextView[@text=\"Downloaded\"]"));
+        downloadSections.click();
+
+        WebElement downloadedArticle = driver.findElement(By.xpath("//android.widget.ImageView"));
+        downloadedArticle.click();
+
+        WebElement sectionsView = driver.findElement(By.xpath("//android.view.ViewGroup[@content-desc=\"Tap me to open the sections menu\"]/android.view.ViewGroup/android.view.ViewGroup"));
+        Assert.assertTrue(sectionsView.isDisplayed());
+
 		
 		
 	}
 	
 	@Test
 	public void NavigateToSelectedPageInArticle() {
-		
+        WebElement newsView = driver.findElement(By.xpath("//android.widget.TextView[@text=\"News\"]"));
+        newsView.click();
+		WebElement squeakCleanNewsArticle = driver.findElement(By.xpath("//androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout"));
+        squeakCleanNewsArticle.click();
 		
 		
 
@@ -66,7 +78,8 @@ public class OnlineImagesAppTest {
 
 	@Test
 	public void goToImageGallery() {
-		
+		WebElement viewImagesIcon = driver.findElement((By.xpath("//android.widget.ImageView[@content-desc=\"View images\"]")));
+        viewImagesIcon.click();
 		
 	}
 
